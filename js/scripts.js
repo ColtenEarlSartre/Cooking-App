54dfb61470ca6ad007413f828590ce0261be93b5
Student <example@example.com>
2019-10-21 11:30:32 -0700

    create initial html, js for cooking-app

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var gallon = parseInt(prompt("How many gallons?"))
+
+var liter = gallon / 3.78541;
+alert(liter)
+
+var liter = parseInt(prompt("How many liters?"))
+
+var gallon = liter / 0.264172;
+alert(gallon)
